b8ed9629b0eb073859d3bdc53b16b6720f411207
Mahmoud Aoata <mdo@Mahmouds-MacBook-Pro.local>
2022-10-24 13:12:17 +0200

    xcode project files

---
+++ swiftui-notification/swiftui-notification/swiftui_notificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_notificationApp.swift
+//  swiftui-notification
+//
+//  Created by Mahmoud Aoata on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_notificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
